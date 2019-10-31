99240141ed6fdbf34b8e6bde0a066d533c8a4997
Tucker <trblackw@ncsu.edu>
2019-10-31 18:45:14 -0400

    Set up base for egg selection

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes: [String : Int] = [
+        "Soft" : 5,
+        "Medium" : 7,
+        "Hard" : 12
+    ]
     
+   
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        print(eggTimes[hardness!]!)
+    }
 }
